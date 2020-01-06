9acba63bc309fe2f652926049bb823fdc3157e9d
phil cowan <philipjcowan1@gmail.com>
2020-01-06 12:54:19 -0700

    writing some pseudo js

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+//writes functionality for the start button, to begin the game
+
+
+//starts a 30 second timer for each new question
+
+
+//create an array of objects each object will be a question and its key value pairs will be incorrect answers with one correct answer
+var question = {
+    "What kind of bear is best?";
+}
