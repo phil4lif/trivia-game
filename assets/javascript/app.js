a3427afb7699738ab7537a5442657f0fd20e9764
phil cowan <philipjcowan1@gmail.com>
2020-01-07 23:49:00 -0700

    writes more functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,10 @@
+//declare some variables for scorekeeping
+var correctCounter = 0;
+var wrongCounter = 0;
+
+
+
+
 //starts a 30 second timer for each new question
 var timer = 30
 var intervalId
@@ -16,28 +23,39 @@ function decrement() {
         $("#result").text("Time's Up!")
     }
 }
+function wrong(){
+    wrongCounter++;
+    $("#result").text("Incorrect")
+    $("#wrongcounter").text(wrongCounter);
+}
+function correct(){
+    correctCounter++;
+    $("#result").text("Correct!")
+    $("#correctcounter").text(correctCounter);
+}
 
 //create an array of objects each object will be a question and its key value pairs will be incorrect answers with one correct answer
 var questions = [
     {
         question: "What kind of bear is best?",
         correctAnswer: 0,
-        Answers: ["There are basically two schools of thought", "Black Bears", "Grizzly Bears", "Polar Bears"]
+        answers: ["There are basically two schools of thought", "Black Bears", "Grizzly Bears", "Polar Bears"]
     }, {
         question: "What does Dwight grow on his farm?",
         correctAnswer: 0,
-        Answers: ["Beets", "Radishes", "Corn", "Pecans"]
+        answers: ["Beets", "Radishes", "Corn", "Pecans"]
     }, {
         question: "What is the title of the action film that Michael directs and stars in?",
         correctAnswer: 1,
-        Answers: ["The Scranton Strangler", "Threat Level Midnight", "Scranton, The Electric City", "An American Workplace"]
+        answers: ["The Scranton Strangler", "Threat Level Midnight", "Scranton, The Electric City", "An American Workplace"]
     }
 ]
 
 //write a function to display the current question as well as the correct and incorrect answers
 function newQuestion(){
-    
+    timer = 30;
     $("#questiondiv").text(questions[0].question)
+    $(".answerdiv").text(questions[0].answers)
 }
 //writes functionality for the start button, to begin the game
 $("#startbutton").on("click", run);
