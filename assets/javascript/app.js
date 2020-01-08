e4ac2162dece7c82db697466c833730fe8ed9ef8
phil cowan <philipjcowan1@gmail.com>
2020-01-07 22:40:29 -0700

    gives functionality to the start div and timer div

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,39 @@
-//writes functionality for the start button, to begin the game
-
-
 //starts a 30 second timer for each new question
+var timer = 30
+var intervalId
 
+function run() {
+    clearInterval(intervalId);
+    intervalId = setInterval(decrement, 1000);
+}
+//  The decrement function.
+function decrement() {
+    //  Decrease timer by one.
+    timer--;
+    //  Show the number in the #timer tag.
+    $("#timer").html("<h3>" + timer + "</h3>");
+    if (timer === 0) {
+        $("#result").text("Time's Up!")
+    }
+}
 
 //create an array of objects each object will be a question and its key value pairs will be incorrect answers with one correct answer
 var questions = [
     {
-    question: "What kind of bear is best?",
-    correctAnswer: "There are basically two schools of thought",
-    wrongAnswers: ["Black Bears", "Grizzly Bears", "Polar Bears"]
-},{
-    question: "What does Dwight grow on his farm?",
-    correctAnswer: "Beets",
-    wrongAnswers: ["Radishes", "Corn", "Pecans"]
-},{
-    question: "What is the title of the action film that Michael directs and stars in?",
-    correctAnswer: "Threat Level Midnight",
-    wrongAnswers: ["The Scranton Strangler", "Scranton, The Electric City", "An American Workplace"]
-}
+        question: "What kind of bear is best?",
+        correctAnswer: "There are basically two schools of thought",
+        wrongAnswers: ["Black Bears", "Grizzly Bears", "Polar Bears"]
+    }, {
+        question: "What does Dwight grow on his farm?",
+        correctAnswer: "Beets",
+        wrongAnswers: ["Radishes", "Corn", "Pecans"]
+    }, {
+        question: "What is the title of the action film that Michael directs and stars in?",
+        correctAnswer: "Threat Level Midnight",
+        wrongAnswers: ["The Scranton Strangler", "Scranton, The Electric City", "An American Workplace"]
+    }
 ]
+
+
+//writes functionality for the start button, to begin the game
+$("#startbutton").on("click", run);
