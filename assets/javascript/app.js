4d1d26e222404fafca1e6735170d17955e5ba6d9
phil cowan <philipjcowan1@gmail.com>
2020-01-07 22:22:15 -0700

    starts writing the array of question objects

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,6 +5,18 @@
 
 
 //create an array of objects each object will be a question and its key value pairs will be incorrect answers with one correct answer
-var question = {
-    "What kind of bear is best?";
+var questions = [
+    {
+    question: "What kind of bear is best?",
+    correctAnswer: "There are basically two schools of thought",
+    wrongAnswers: ["Black Bears", "Grizzly Bears", "Polar Bears"]
+},{
+    question: "What does Dwight grow on his farm?",
+    correctAnswer: "Beets",
+    wrongAnswers: ["Radishes", "Corn", "Pecans"]
+},{
+    question: "What is the title of the action film that Michael directs and stars in?",
+    correctAnswer: "Threat Level Midnight",
+    wrongAnswers: ["The Scranton Strangler", "Scranton, The Electric City", "An American Workplace"]
 }
+]
