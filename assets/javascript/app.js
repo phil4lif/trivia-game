d4c35c7eafa8ef4e10d87c1c4d0beaa4e2196d61
phil cowan <philipjcowan1@gmail.com>
2020-01-09 23:05:29 -0700

    worked the rest of the javascript out, it needs to be refactored

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,33 +2,45 @@
 var correctCounter = 0;
 var wrongCounter = 0;
 var questionNumber = 0;
-var correctAnswer ;
+var correctAnswer;
 
+//write a function for the end of the game, that is for when the user has answered the last question. This function will be invoked once the question number exceeds the length of the questions array
+function endGame() {
+    console.log("gameover")
+    $(".game").hide();
+    $("#scoreboard").show();
+    timer = 30;
+    // $("#result").text("Final Score")
+}
 
 //starts a 30 second timer for each new question
 var timer = 30
 var intervalId
 
 function run() {
+    $("#startbutton").hide();
+    $(".game").show();
     clearInterval(intervalId);
     intervalId = setInterval(decrement, 1000);
 }
 //  The decrement function.
 function decrement() {
     //write an if so that the timer does not go negative
-    if(timer>0){
-    //  Decrease timer by one.
-    timer--;
-    //  Show the number in the #timer tag.
-    $("#timer").html("<h3>" + timer + "</h3>");
-    if (timer === 0) {
-        $("#result").text("Time's Up!")
-        wrongCounter++;
-        $("#wrongcounter").text("Wrong Answers: " + wrongCounter);
-        questionNumber++
-        newQuestion(questionNumber);
+    if (timer > 0) {
+        //  Decrease timer by one.
+        timer--;
+        //  Show the number in the #timer tag.
+        $("#timer").html("<h3>Time Remaining: " + timer + "</h3>");
+        if (timer === 0) {
+            $(".game")
+            $("#result").text("Time's Up!")
+            wrongCounter++;
+            $("#wrongcounter").text("Wrong Answers: " + wrongCounter);
+            questionNumber++
+            console.log(questionNumber, " ", (questions.length-1))
+            newQuestion(questionNumber);
+        }
     }
-}
 }
 //create an array of objects each object will be a question and its key value pairs will be incorrect answers with one correct answer
 var questions = [
@@ -63,52 +75,92 @@ var questions = [
     }
 ]
 
+
 //write a function to display the current question as well as the correct and incorrect answers
 function newQuestion(questionNumber) {
     timer = 30;
+    if(questionNumber<(questions.length-1)){
     $("#questiondiv").text(questions[questionNumber].question);
     $("#answer-one").text(questions[questionNumber].answers[0]);
     $("#answer-two").text(questions[questionNumber].answers[1]);
     $("#answer-three").text(questions[questionNumber].answers[2]);
     $("#answer-four").text(questions[questionNumber].answers[3]);
-
+    }
     // $(".answerdiv").text(questions[0].answers)
 }
 
 //write a click function for the answers 
 $(".answerdiv").on("click", function () {
-    
-    // console.log(this);
+    console.log(questionNumber)
+    console.log(this);
     var answer = $(this).attr("data-answer")
     // console.log(answer);
+    //check if the question number is valid or if the game should be over
+
     //if the answer is correct
     if (answer == questions[questionNumber].correctAnswer) {
         //correct will be displayed
-        $("#result").text("Correct")
-        //correctcounter is incremented
-        correctCounter++;
+        $("#solution").show()
+        $(".game").hide()
+        questionNumber++;
+          //correctcounter is incremented
+          correctCounter++;
+        setTimeout(function () {
+            $("#solution").hide()
+            $(".game").show()
+            timer = 30;
+            if(questionNumber===questions.length-1){
+                endGame();
+            }
+        }, 200)
+      
         //and the correctcounter is displayed
         $("#correctcounter").text("Correct Answers: " + correctCounter);
         //the question number is incremented
-        questionNumber++
+     
         //the new question function is invoked with the new number passed in
-        newQuestion(questionNumber);
-    //otherwise the answer is incorrect
-    }else{
+        console.log(questionNumber, " ", (questions.length-1))
+
+        if (questionNumber < (questions.length-1)) {
+            newQuestion(questionNumber);
+        } else {
+            endGame();
+        }
+        //otherwise the answer is incorrect
+    } else {
+        $("#solution").show()
+        $(".game").hide()
+        questionNumber++;
+        setTimeout(function () {
+            $("#solution").hide()
+            $(".game").show()
+            timer = 30;
+            if(questionNumber===questions.length-1){
+                endGame();
+            }
+        }, 200)
         // console.log("incorrect")
         //incorrect is displayed
-        $("#result").text("Sorry, Incorrect")
+        // $("#result").text("Sorry, Incorrect")
         //the wrong counter variable is incremented
         wrongCounter++
         //the wrong counter is displayed on the page
         $("#wrongcounter").text("Wrong Answers: " + wrongCounter);
         //the question number is incremented
-        questionNumber++
+       
         //and the newquestion function is invoked with the new number passed in
-        newQuestion(questionNumber);
+        console.log(questionNumber, " ", (questions.length-1))
+        if (questionNumber < (questions.length-1)) {
+            newQuestion(questionNumber);
+        } else {
+            endGame()
+        }
     }
 });
-
+$("#solution").hide()
+$("#scoreboard").hide()
+$(".game").hide()
+$("#starbutton").show()
 //if i is incorrect the wrongcounter will increase
 //writes functionality for the start button, to begin the game
 $("#startbutton").on("click", run);
